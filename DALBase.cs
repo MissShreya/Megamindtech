f248d48bbbe7a886b9bf4a3919698ea7af4c4cf1
MissShreya <shreyanshisrivastava75@gmail.com>
2024-10-04 01:25:26 +0530

    Create some method for  ADO.Net Connectivity # DALBase Class Documentation
    ## Overview
    The DALBase class is a data access layer (DAL) component designed to facilitate interactions with a SQL Server database. It encapsulates methods for establishing and closing database connections, as well as executing stored procedures and retrieving results in a DataSet.
    
    ## Dependencies
    - System.Data.SqlClient
    - System.Configuration
    
    ## Constructor
    ### DALBase()
    Initializes a new instance of the DALBase class and retrieves the database connection string from the application configuration settings.
    
    ## Methods
    
    ### void Create_Connection()
    Establishes a connection to the SQL Server database using the connection string provided in the application configuration.
    
    - **Behavior**:
      - If the connection is closed or null, it attempts to open the connection.
      - Swallows exceptions silently, which may need to be addressed for better error handling.
    
    ### void Close_Connection()
    Closes the current connection to the database if it is open.
    
    - **Behavior**:
      - Sets the connection object to null after closing.
      - Throws exceptions that occur during closing.
    
    ### System.Data.DataSet ExecuteProcedure(string SPName, string[] pName, string[] pValue)
    Executes a stored procedure in the SQL Server database and returns the result set as a DataSet.
    
    - **Parameters**:
      - string SPName: The name of the stored procedure to be executed.
      - string[] pName: An array of parameter names for the stored procedure.
      - string[] pValue: An array of parameter values corresponding to the parameter names.
    
    - **Returns**:
      - A DataSet containing the result set from the executed stored procedure.
    
    - **Behavior**:
      - Opens a database connection, prepares the SQL command with parameters, and fills the DataSet with the results.
      - Swallows exceptions silently, which may need to be addressed for better error handling.
      - Ensures the connection is closed in the inally block.

--- DALBase.cs
+++ DALBase.cs
@@ -1,11 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace MEGAMINDTECH
 {
-    public class DALBase
+    internal class DALBase
     {
+        private string strCon = "";
+        private System.Data.SqlClient.SqlConnection objConnection;
+        private System.Data.DataSet dsResultSet;
+
+        public DALBase()
+        {
+            this.strCon = System.Configuration.ConfigurationManager.AppSettings["DBcon"].ToString();
+        }
+
+        public void Create_Connection()
+        {
+            this.objConnection = null;
+            try
+            {
+                this.objConnection = new System.Data.SqlClient.SqlConnection(this.strCon);
+                if (this.objConnection.State == System.Data.ConnectionState.Closed || this.objConnection == null)
+                {
+                    this.objConnection.Open();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void Close_Connection()
+        {
+            try
+            {
+                if (this.objConnection.State == System.Data.ConnectionState.Open || this.objConnection != null)
+                {
+                    this.objConnection.Close();
+                    this.objConnection = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public System.Data.DataSet ExecuteProcedure(string SPName, string[] pName, string[] pValue)
+        {
+            this.dsResultSet = null;
+            try
+            {
+                this.dsResultSet = new System.Data.DataSet();
+                this.Create_Connection();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = this.objConnection;
+                sqlCommand.CommandText = SPName;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                for (int i = 0; i < pName.Length; i++)
+                {
+                    sqlCommand.Parameters.AddWithValue(pName[i], string.IsNullOrEmpty(pValue[i]) ? null : pValue[i]);
+                }
+                sqlDataAdapter.SelectCommand = sqlCommand;
+                sqlDataAdapter.Fill(this.dsResultSet);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+            finally
+            {
+                this.Close_Connection();
+            }
+            return this.dsResultSet;
+        }
+
+
     }
 }
