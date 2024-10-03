7bc5e7babee598398d609d0cf1d4dcd8efef8d67
MissShreya <shreyanshisrivastava75@gmail.com>
2024-10-04 01:10:10 +0530

    Add Controller MegaTask and create view Index.

---
+++ Controllers/MegaTaskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEGAMINDTECH.Controllers
+{
+    public class MegaTaskController : Controller
+    {
+        // GET: MegaTask
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
